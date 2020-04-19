8c41f777be6c7990f3543ee23bb23438c32a9203
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-19 18:30:47 -0400

    add axios to get data from mocked service

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 export default {
+  data() {
+    return {
+      events: []
+    }
+  },
   components: {
     EventCard
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
