d87746a414af13990c193ae651ed9096dde7b872
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-19 22:13:44 -0400

    move user and event states to modules and update views

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link :to="{ name: 'event-list', query: { page: page - 1 } }"
         >Prev</router-link
@@ -32,7 +32,7 @@ export default {
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    ...mapState(['event', 'user'])
   }
 }
 </script>
