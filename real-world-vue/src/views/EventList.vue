3dbe7b1f3d46291427c0358ce1489932a52a18cf
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-19 21:37:48 -0400

    add fetched events to state and add pagination to event listing

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -2,29 +2,37 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }"
+        >Prev</router-link
+      >
+      |
+    </template>
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
+      >Next</router-link
+    >
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
+
 export default {
-  data() {
-    return {
-      events: []
-    }
-  },
   components: {
     EventCard
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events'])
   }
 }
 </script>
