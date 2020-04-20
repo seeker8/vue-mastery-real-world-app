da11c307f02a4487325cb15053354cdcfdc1c092
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-19 21:11:41 -0400

    add create event function and redirect to evetn details

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -29,7 +29,7 @@ export default {
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
 
