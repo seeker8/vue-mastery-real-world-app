87d972ded48a810ea13b65a292930904095b679d
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-20 15:51:54 -0400

    remove vuex dependency from show event component

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
-  props: ['id'],
-  created() {
-    this.$store.dispatch('fetchEvent', this.id)
-  },
-  computed: mapState(['event'])
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
 
