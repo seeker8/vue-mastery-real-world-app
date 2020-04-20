3d0dc3d5d29572e4c2a2e184ccb2fbe3fd26679e
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-20 14:53:20 -0400

    handle progress bar on router

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventCreate from '../views/EventCreate.vue'
 import EventShow from '../views/EventShow.vue'
+import NProgress from 'nprogress'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -21,7 +23,12 @@ const routes = [
     path: '/event/:id',
     name: 'event-show',
     component: EventShow,
-    props: true
+    props: true,
+    beforeEnter(routeTo, routeFrom, next) {
+      store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
+        next()
+      })
+    }
   }
 ]
 
@@ -29,6 +36,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
